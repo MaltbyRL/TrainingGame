ff586d7b9fb035cdb76f0954d69d6ec1b2880296
MaltbyRL <RLMaltby@gmail.com>
2017-11-24 21:05:24 -0800

    testing SQLite

--- CompTIA/220-901/Hardware/aPlusTerminology.cs
+++ CompTIA/220-901/Hardware/aPlusTerminology.cs
@@ -1,62 +1,379 @@
-struct terminologyAndTechnology
-{ //Below aarray of common acronyms abrieviations and their deffinition if index is empty
-  // reenter name ['acronyms / abrieviations', 'fullname / title', deffinition, 'comma separated tags i.e. ("220-901', 'Hardware', 'BIOS") '];
-    public string[] Question = "Complete the following deffinitions"; // Actual question text.
 
-    public string[] definitionBIOS = ['BIOS', 'Basic Input OutPut System', ''
-    , 'Hardware, BIOS, '];    // Array of answers from which user can choose.
-    public string[] definitionCMOS = ['CMOS','Complemetary Metal Oxide Semiconductor', ''
-    , 'Hardware, Acronym, CMOS'];    // Array of answers from which user can choose.
-    public string[] definitionCPU = ['CPU', 'Central Processing Unit', 'Usually a thin waffer like square of silicon and tiny transistors, \n by which the majority of processing tasks are completed.'
-    , 'Hardware, Acronym, CPU'];    // Array of answers from which user can choose.
-    public string[] definitionDVI = ['DVI', '', 'A connector used for output to the monitor.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitioneSATA = ['eSATA', 'eSATA', 'Provides a variant of SATA meant for external connectivity. It uses a more robust connector, longer shielded cables, and stricter (but backward-compatible) electrical standards.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionFirewire = ['Firewire', 'Firewire', 'Firewire is a serial bus architecture for high-speed data transfer.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionGPU = ['GPU', 'Graphics Processing Unit', 'A graphics processing unit (GPU) is a specialized electronic circuit designed to rapidly manipulate and alter memory to accelerate the creation of images in a frame buffer intended for output to a display device.'
-    , 'Hardware, Acronym, GPU'];    // Array of answers from which user can choose.
-    public string[] definitionHDMI = ['HDMI', 'High-Definition Multimedia Interface', 'Proprietery interfaces use to output audio and video to a monitor.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionInput = ['Input', 'Input', 'Starting a computing action by doing something, typing text, clicking a mouse or manipulating a touchscreen.'
-    , 'Software, Input'];    // Array of answers from which user can choose.
-    public string[] definitionMiniDIN = ['Mini-DIN', 'Mini-DIN', 'A connector used for a keyboard or a mouse.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionMOBO = ['MOBO', 'Motherboard', 'is the main printed circuit board (PCB) found in general purpose microcomputers and other expandable systems.\n It holds and provides communication to many of the components needed for operating a computer.'
-    , 'Hardware, Acronym, MOBO'];    // Array of answers from which user can choose.
-    public string[] definitionNIC = ['NIC', 'Network Interface Card', 'A NIC is a circuit board or card that is installed in a computer so that it can be connected to a network.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionPC = ['PC', 'Personal Computer', 'a PC is an electronic device that can perform calculations. \n Most common types use special programming languages that certian people write, \n and compile to perform specific tasks.'
-    , 'Hardware, Acronym, PC'];    // Array of answers from which user can choose.
-    public string[] definitionPCB = ['PCB', 'Printed Circuit Board', 'A printed circuit board (PCB) mechanically supports and electrically connects electronic components using\n conductive tracks, pads and other features etched from copper sheets laminated onto a non-conductive substrate. Components are normally soldered onto PCBs.'
-    , 'Hardware, Acronym, PCB'];    // Array of answers from which user can choose.
-    public string[] definitionPS2 = ['PS/2', '', 'A connector used for a keyboard or a mouse.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionPSU = ['PSU', 'Power Supply Unit', ''
-    , 'Hardware, Acronym, PSU'];    // Array of answers from which user can choose.
-    public string[] definitionRAM = ['RAM', 'Random Access Memory', ''
-    , 'Hardware, Acronym, RAM' ];    // Array of answers from which user can choose.
-    public string[] definitionSPDIF = ['S/PDIF', 'Sony/Philips Digital Interface Format', 'A type of connection used in consumer audio equipment to output audio over reasonably short distances.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionSSD = ['SSD', 'Solid State Drive', 'A device that uses integrated circuit assemblies as memory to store data persistently. SSDs have no moving mechanical parts.'
-    , 'Hardware, Acronym, SSD'];    // Array of answers from which user can choose.
-    public string[] definitionUEFI = ['UEFI', 'Unified Extensible Firmware Interface', 'IT is a specification that defines a software interfaces between an operating system and platform firmware. \n "It\'s a newer verson of the BIOS!". '
-    , 'Acronym, UEFI'];    // Array of answers from which user can choose.
-    public string[] definitionUI = ['UI', 'User Interface', 'Often appears as a collection of windows or icons that a user can interact with.'
-    , 'Acronym, '];    // Array of answers from which user can choose.
-    public string[] definitionUSB = ['USB', '', 'A USB connector used for multiple Input or Output devices.'
-    , ''];    // Array of answers from which user can choose.
-    public string[] definitionVGA = ['VGA', '', ''
-    , ''];    // Array of answers from which user can choose.
+CREATE TABLE aPlusTerm
+(
+  terminologyID STRING PRIMARY KEY UNIQUE NOT NULL
+, terminologyAcronym STRING UNIQUE
+, terminologyName STRING NOT NULL
+, terminologyNotes STRING DEFAULT NULL
+)
 
 
-    public string[] definition = ['', '', ''
-    , ''];    // Array of answers from which user can choose.
-    public string[] definition = ['', '', ''
-    , ''];    // Array of answers from which user can choose.
-    public string[] definition = ['', '', ''
-    , ''];    // Array of answers from which user can choose.
+;
 
-    public string notes = ""; // Notes on subject matter.
-}
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionBIOS'
+, 'BIOS'
+, 'Basic Input OutPut System'
+, 'Notes: '
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionCMOS'
+, 'CMOS'
+, 'Complemetary Metal Oxide Semiconductor'
+, 'Notes: '
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionCPU'
+, 'CPU'
+, 'Central Processing Unit'
+, 'Usually a thin waffer like square of silicon and tiny transistors, \n by which the majority of processing tasks are completed.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionDVI'
+, 'DVI'
+, 'Notes: '
+, 'A connector used for output to the monitor.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitioneSATA'
+, 'eSATA'
+, 'Notes: '
+, 'Provides a variant of SATA meant for external connectivity. It uses a more robust connector, longer shielded cables, and stricter (but backward-compatible) electrical standards.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionFirewire'
+, 'Firewire'
+, 'Firewire'
+, 'Firewire is a serial bus architecture for high-speed data transfer.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionGPU'
+, 'GPU'
+, 'Graphics Processing Unit'
+, 'A graphics processing unit (GPU) is a specialized electronic circuit designed to rapidly manipulate and alter memory to accelerate the creation of images in a frame buffer intended for output to a display device.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionHDMI'
+, 'HDMI'
+, 'High-Definition Multimedia Interface'
+, 'Proprietery interfaces use to output audio and video to a monitor.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionInput'
+, 'Input'
+, 'Input'
+, 'Starting a computing action by doing something, typing text, clicking a mouse or manipulating a touchscreen.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionMiniDIN'
+, 'Mini-DIN'
+, 'Mini-DIN'
+, 'A connector used for a keyboard or a mouse.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionMOBO'
+, 'MOBO'
+, 'Motherboard'
+, 'is the main printed circuit board (PCB) found in general purpose microcomputers and other expandable systems.\n It holds and provides communication to many of the components needed for operating a computer.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionNIC'
+, 'NIC'
+, 'Network Interface Card'
+, 'A NIC is a circuit board or card that is installed in a computer so that it can be connected to a network.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionPC'
+, 'PC'
+, 'Personal Computer'
+, 'A PC is an electronic device that can perform calculations. \n Most common types use special programming languages that certian people write, \n and compile to perform specific tasks.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionPCB'
+, 'PCB'
+, 'Printed Circuit Board'
+, 'A printed circuit board (PCB) mechanically supports and electrically connects electronic components using\n conductive tracks, pads and other features etched from copper sheets laminated onto a non-conductive substrate. Components are normally soldered onto PCBs.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionPS2'
+, 'PS/2'
+, 'Notes: '
+, 'A connector used for a keyboard or a mouse.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionPSU'
+, 'PSU'
+, 'Power Supply Unit'
+, 'Notes: '
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionRAM'
+, 'RAM'
+, 'Random Access Memory'
+, 'Notes: '
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionSPDIF'
+, 'S/PDIF'
+, 'Sony/Philips Digital Interface Format'
+, 'A type of connection used in consumer audio equipment to output audio over reasonably short distances.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionSSD'
+, 'SSD'
+, 'Solid State Drive'
+, 'A device that uses integrated circuit assemblies as memory to store data persistently. SSDs have no moving mechanical parts.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionUEFI'
+, 'UEFI'
+, 'Unified Extensible Firmware Interface'
+, 'IT is a specification that defines a software interfaces between an operating system and platform firmware. \n "It\'s a newer verson of the BIOS!". '
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionUI'
+, 'UI'
+, 'User Interface'
+, 'Often appears as a collection of windows or icons that a user can interact with.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionUSB'
+, 'USB'
+, 'Notes: '
+, 'A USB connector used for multiple Input or Output devices.'
+)
+;
+insert into aPlusTerm
+(
+  terminologyID
+, terminologyAcronym
+, terminologyName
+, terminologyNotes
+)
+
+values
+(
+'definitionVGA'
+, 'VGA'
+, 'Notes: '
+, 'Notes: '
+)
--- CompTIA/220-901/Hardware/aPlusTrueFalse.cs
+++ CompTIA/220-901/Hardware/aPlusTrueFalse.cs
@@ -1,34 +1,42 @@
 
-struct trueFalse {
-  struct Question1{
-    public string[] Tags = ['Hardware, BIOS']; // Tags the question to a specific area of study.
-    public string[] Question = ["First, the BIOS initializes the system"]; // Actual question text.
-    public string[] Choices = ["True", "False"];    // Array of answers from which user can choose.
-    public int Answer 1;          // Index of correct answer within Choices.
-    public string notes = "The answer is true, the first thing a BIOS does is initialize your system,
-    -Making sure you have all hardware components needed to continue loading an Operating System (OS).
-    -Checks the cpu is installed and operating properly
-    -Checks the memory is installed and operating properly."; // Notes on subject matter.
-  }
-  struct Question2{
-    public string[] Tags = ['Hardware, Stages']; // Tags the question to a specific area of study.
-    public string[] Question = ["The 'computing process' describes the 3 stages computers work through. Input, Processing, and Output."]; // Actual question text.
-    public string[] Choices = ["True", "False"];    // Array of answers from which user can choose.
-    public int Answer 1;          // Index of correct answer within Choices.
-    public string notes = "The 3 stages in the computing process are: input, processing, and output. \/n In stage 1, input, you are issuing a command to the computer. \/n In stage 2, processing, it's usually the computers CPU thats working to complete the issued task. \/n In stage 3, output, on completion of the processing stage, the computer may play a sound or change the information displayed on screen. \/n Additionally modern computers will typically have 'Data Storage' and 'Network Connection' as additional stages."; // Notes on subject matter.
-  }
-  struct Question3]{
-    public string[] Tags = ['']; // Tags the question to a specific area of study.
-    public string[] Question = [""]; // Actual question text.
-    public string[] Choices = ["True", "False"];    // Array of answers from which user can choose.
-    public int Answer 1;          // Index of correct answer within Choices.
-    public string notes = ""; // Notes on subject matter.
-  }
-  struct Question4{
-    public string[] Tags = ['']; // Tags the question to a specific area of study.
-    public string[] Question = [""]; // Actual question text.
-    public string[] Choices = ["True", "False"];    // Array of answers from which user can choose.
-    public int Answer 1;          // Index of correct answer within Choices.
-    public string notes = ""; // Notes on subject matter.
-  }
-}
+insert into aPlusTerminologyDB
+  (
+    questionID
+  , questionTags
+  , questionType
+  , questionAttempts
+  , questionCorrect
+  , questionIncorrect
+  , questionText
+  , questionAnswer
+  , questionChoices
+  , questionNotes
+)
+
+values
+(
+    201
+  , 'Hardware, BIOS'
+  , 'TrueFalse'
+  , 0
+  , 0
+  , 0
+  ,'First, the BIOS initializes the system'
+  , 1
+  , 'True, False'
+  , 'The answer is true, the first thing a BIOS does is initialize your system,\n -Making sure you have all hardware components needed to continue loading an Operating System (OS).\n -Checks the cpu is installed and operating properly.\n -Checks the memory is installed and operating properly.'
+)
+
+values
+(
+    202
+  , 'Hardware, Stages'
+  , 'TrueFalse'
+  , 0
+  , 0
+  , 0
+  ,'The "computing process" describes the 3 stages computers work through. Input, Processing, and Output.'
+  , 1
+  , 'True, False'
+  , 'The 3 stages in the computing process are: input, processing, and output. \/n In stage 1, input, you are issuing a command to the computer. \/n In stage 2, processing, it\'s usually the computers CPU thats working to complete the issued task. \/n In stage 3, output, on completion of the processing stage, the computer may play a sound or change the information displayed on screen. \/n Additionally modern computers will typically have "Data Storage" and "Network Connection" as additional stages.'
+)
